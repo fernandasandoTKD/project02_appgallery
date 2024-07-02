7ed18f0793e3ac2b7227143d6a5f87dd2eab88ee
Fernanda Sandoval <luisa.sandoval@commity.co>
2024-07-02 06:45:04 -0500

    PR03:Components simple navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Taekwondo1 } from './components/Taekwondo1'
 import { Taekwondo2 } from './components/Taekwondo2'
@@ -8,18 +5,24 @@ import { Taekwondo3 } from './components/Taekwondo3'
 import { Taekwondo4 } from './components/Taekwondo4'
 import { Taekwondo5 } from './components/Taekwondo5'
 import { Taekwondo6 } from './components/Taekwondo6'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-     <Taekwondo1></Taekwondo1>
-     <Taekwondo2></Taekwondo2>
-     <Taekwondo3/>
-     <Taekwondo4/>
-     <Taekwondo5></Taekwondo5>
-     <Taekwondo6></Taekwondo6>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/taekwondo1" element= {<Taekwondo1/>}/>
+        <Route path="/taekwondo2" element= {<Taekwondo2/>}/>
+        <Route path="/taekwondo3" element= {<Taekwondo3/>}/>
+        <Route path="/taekwondo4" element= {<Taekwondo4/>}/>
+        <Route path="/taekwondo5" element= {<Taekwondo5/>}/>
+        <Route path="/taekwondo6" element= {<Taekwondo6/>}/>
+      </Routes>
+    </BrowserRouter>
+    
     </>
   )
 }
